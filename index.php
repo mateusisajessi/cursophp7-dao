fe9c4abab30a4f21ff3030f5d667b94e957b2e47
Mateus Santana <mateus.isajessi@gmail.com>
2019-02-07 21:06:12 -0200

    metodos de lista

--- index.php
+++ index.php
@@ -2,12 +2,27 @@
 
 	require_once("config.php");
 
+	//Carrega todos os dados da tabela
 	// $sql= new Sql();
 	// $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 	// echo json_encode($usuarios);
 
-	$root = new Usuario();
-	$root->loadById(3);
-	echo $root;
+	//Carrega um usuário
+	// $root = new Usuario();
+	// $root->loadById(3);
+	// echo $root;
+
+	//Carrega uma lista de usuários
+	// $lista = Usuario::getList();
+	// echo json_encode($lista);
+
+	//Carrega uma lista de usuários buscando pelo login
+	// $search = Usuario::search("m");
+	// echo json_encode($search);
+
+	//Carrega um usuario usando o login e a senha
+	$usuario = new Usuario();
+	$usuario->login("mateus", "12345678");
+	echo $usuario;
 
 ?>
